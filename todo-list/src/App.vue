fbb31ebccfc1b1ee2a65f7e718f9fbc8baff12c6
glaucia86 <glaucia_lemos86@hotmail.com>
2020-06-22 19:55:48 -0300

    Atualizando o arquivo 'App.vue'

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -4,7 +4,7 @@
       <div class="ui text container">
         <h1 class="ui inverted header"> Aplicação Todo List com Vue.js 3</h1>
         <h2 class="ui inverted header">Glaucia Lemos
-          <div class="sub header">Cloud Advocate @ Microsoft</div>
+          <div class="sub header">Cloud Advocate em JavaScript/Node.js @ Microsoft</div>
         </h2>
         <h3 class="ui inverted header">Rio de Janeiro, Brasil - 2020</h3>
         <a class="tiny ui youtube button" type="button" href="https://www.youtube.com/user/l32759">
